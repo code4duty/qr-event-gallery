33f2270d7b3ee5aa1fd0c74b1e686dc111c79c1a
code4duty <snehamoy14@gmail.com>
2026-07-08 15:43:10 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -351,7 +351,12 @@ document.addEventListener('DOMContentLoaded', () => {
             const btnDl = document.createElement('button');
             btnDl.className = 'btn-card-action download';
             btnDl.title = 'Download';
-            btnDl.innerHTML = '<svg ...></svg>';
+            btnDl.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+            <polyline points="7 10 12 15 17 10"/>
+            <line x1="12" y1="15" x2="12" y2="3"/>
+            </svg>`;
 
             btnDl.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -365,7 +370,14 @@ document.addEventListener('DOMContentLoaded', () => {
                const btnDel = document.createElement('button');
                btnDel.className = 'btn-card-action delete';
                btnDel.title = 'Delete Media';
-               btnDel.innerHTML = '<svg ...></svg>';
+               btnDel.innerHTML = `
+               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+               <polyline points="3 6 5 6 21 6"/>
+               <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/>
+               <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+               <line x1="10" y1="11" x2="10" y2="17"/>
+               <line x1="14" y1="11" x2="14" y2="17"/>
+               </svg>`;
 
                btnDel.addEventListener('click', (e) => {
                  e.stopPropagation();
